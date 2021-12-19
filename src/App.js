c1e30d874a36ec1eb0326ee9963b9ce468d5b797
shalini47ch <shalini47choudhary@gmail.com>
2021-12-19 17:48:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const images = [
+  {
+    name: "Image 1",
+    url: "https://media.istockphoto.com/photos/artificial-intelligence-and-communication-network-concept-picture-id1281297414?b=1&k=20&m=1281297414&s=170667a&w=0&h=YgXut9DQqJDDfl3fmT93kLnaWJTFJP9Umg-Tefw3BPs=",
+  },
+  {
+    name: "Image 2",
+    url: "https://media.istockphoto.com/photos/close-up-of-hand-holding-smartphone-and-screen-applications-with-picture-id1278703639?b=1&k=20&m=1278703639&s=170667a&w=0&h=dEOHQ5wlbZWp14I0XTrT6vbGSAgUsqt8I9COp8VCROg=",
+  },
+  {
+    name: "Image 3",
+    url: "https://media.istockphoto.com/photos/high-angle-view-asian-chinese-womans-hand-on-mobile-app-for-online-picture-id1300476665?b=1&k=20&m=1300476665&s=170667a&w=0&h=6yLWL7XmKiiMRheXK4E1cdcPOqp54y3Au-mxdbuodyQ=",
+  },
+];
+
+class App extends React.Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      arrows: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      className: "carousel-slider",
+    };
+    return (
+      <div className="App">
+        <Slider {...settings}>
+          {images.map((image) => {
+            return (
+              <div className="image-slider">
+                <img src={image.url} alt="not found" width="100%" />
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-node-access */
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("checks for the classname in the app component", () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toHaveClass("App");
 });
